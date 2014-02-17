d6f6cf826afcc43eb59bc07388ec6251a1d6e574
Kalina Todorova <kt@natur-energi.dk>
2014-02-17 17:17:21 +0100

    Update send_template: add send_at variable and test functions to it

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject clj-mandrill "0.1.0-SNAPSHOT"
+(defproject clj-mandrill "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                  [clj-http "0.5.5"]
-                  [cheshire "4.0.3"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-time "0.6.0"]
+                 [clj-http "0.5.5"]
+                 [cheshire "4.0.3"]]
   :aot [clj-mandrill.interop]
   :profiles {:dev {:dependencies [[clj-http-fake "0.4.1"]]}})
